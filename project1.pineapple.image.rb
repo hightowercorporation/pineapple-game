78d7365d59e41c2fb5474af87d0ab1a19af1a096
david hightower <w.david.hightower@gmail.com>
2017-10-03 23:36:48 +0000

    second commit

---
+++ project1.pineapple.image.rb
@@ -0,0 +1,47 @@
+                                                                            
+                                                 ``         ``                                      
+                                       `:`      `++       .:`                                       
+                                  `     .+-     /+o`.   `-:``                                       
+                                   ``    -s:   -:++//  .-:.-`                                       
+                                    `.`  `o+:` +.ooo: ----/                                         
+                                      `.` +s++:/.syo-::-`/`   ``.                                   
+                                 `.`    ..:h+:+:-shs//:`-.  .:.`                                    
+                                  `-:.`  .:ho.:::ods+/... `.-`                                      
+                                   `oo--` -oy`---+yo+-./``-`    ```                                 
+                                    `+s/--`:s..:::+/::oy.-`  `.-.`                                  
+                                     `-+/---o+-/++:-.-h+`  -:-`                                     
+                                 `    `.:/-:ohsoo:.-:./.`.::`````                                   
+                                  ``..```.--ohmd/-/o+--.-::/+/-.                                    
+                                    ```--.`.:odds+shs/:.-oo:-.`                                     
+                                    ..-::/:-.-/ydhddo:--o/+:---.                                    
+                                     ``.-/++:-///yhyo+oo-:oso:`                                     
+                                     `-/o+-:y:/o+::+:sy//s/``                                       
+                                     `-:--:/osss+:://++:+////::.                                    
+                                    `.--:+o+/+sysyhys//ooso/-.`                                     
+                                        .:/oyo++sydhshy/ss/:.                                       
+                                       .-:++oyddsssssssooso+`                                       
+                                          `+yoshyhdhhmys+-`                                         
+                                         .-.-osysyhho+/:-`                                          
+                                       `:o+//:++/sy+oo:oo//.:`                                      
+                                     .:o-/oho+/+/++o/++yy/+/o:`                                     
+                                     /oo++++oy+/+o+++oss//:/y/+.                                    
+                                   `-/o/s:-/+syo/:://y+/++/oso//`                                   
+                                   -+/:+oos++-/oy+osss//:/sso+oy/                                   
+                                   :+oo+-o+o--:/syoooy+/ooss//+so.                                  
+                                  .+//o/-+:soossso::/+oh//:+syyos:                                  
+                                  -oo++s/ooo/:+oho//osoho+-/s+/ss+.                                 
+                                  .+++o/-++o/:oosyyys+::ooys/+/sy+`                                 
+                                  //ooo+:/+ysoss/+oyo/::+o+/+oos+o:                                 
+                                  /++oooss/+ooy/::/oyyyss//::syo/o:                                 
+                                  /ooo:+oo::++ysoooss+/:oy+/o/yooo.                                 
+                                 `+oos//oyoooyo++oyy+///+oy/o-+s+/.                                 
+                                  :+ssosy/:sss//+/oysys+o+s+/oos//.                                 
+                                  .os/sos::/+yooyossyy++::/hos/o+/                                  
+                                  .s++o+ysoo+syo+///syo+oooy++:+o/`                                 
+                                  .++soosy/:o+ss//++oyyys+:oyooo/-`                                 
+                                   `+o+o+s/:+/+yyssoosy++/++sys+/                                   
+                                    .+++ooos++oys+//:+ysoos+soso`                                   
+                                    `.//++so/:oos+++ssssyoosso+`                                    
+                                      `-++o++/+o+osooo++ooso+-                                      
+                                      `.:+ossyhyyyyyhyyhyso:-`                                      
+                                        ``..--:::://::--.````                                       
---
+++ writetest.rb
@@ -0,0 +1,18 @@
+File.open('project1.class.image.rb', 'r') do |f1|  
+  while line = f1.gets  
+    puts line  
+  end  
+end  
+
+File.open('project1.cutepineapple.image.rb', 'r') do |f1|  
+  while line = f1.gets  
+    puts line  
+  end  
+end  
+
+File.open('project1.pineapple.image.rb', 'r') do |f1|  
+  while line = f1.gets  
+    puts line  
+  end  
+end  
+
